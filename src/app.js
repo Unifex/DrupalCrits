d4b170ee4976f21dc21e0e01633c1321e1f7e57e
Gold <github@evolved.net.nz>
2015-07-27 06:20:50 +1200

    more crits and the estimated release date
    Criticals are broken up into 4 types. All are listed now.
    Have also added the estimated release date from https://drupalreleasedate.com

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@
 var UI = require('ui');
 var ajax = require('ajax');
 //var Vector2 = require('vector2');
-var URL = 'http://drupalreleasedate.com/data/samples.json?limit=1';
 
 var criticalsCard = new UI.Card({
   title: 'Drupal Crits',
@@ -25,18 +24,29 @@ criticalsCard.on('click', 'up', function(e) {
 });
 
 function updateCard () {
+  var URL = 'http://drupalreleasedate.com/data/samples.json?limit=1';
+  
   criticalsCard.subtitle('checking issues');
   criticalsCard.body('Crits: ');
 
   ajax({url: URL, type: 'json'},
     function(json) {
       // Data is supplied here.
+      console.log(json);
       var d = json.data[0];
+      console.log (d);
       var crits = d.critical_bugs + d.critical_plans + d.critical_rtbc + d.critical_tasks;
-      criticalsCard.subtitle('');
-      criticalsCard.body('Crits: ' + crits);
+      criticalsCard.subtitle(d.when.split('T')[0]);
+      criticalsCard.body('Crits: ' + crits + 
+                         "\nBugs: " + d.critical_bugs +
+                         "\nPlans: " + d.critical_plans +
+                         "\nTasks: " + d.critical_tasks +
+                         "\nRTBC: " + d.critical_rtbc
+                        );
     },
     function(error, statusCode) {
+      console.log (statusCode);
+      console.log (error);
       criticalsCard.subtitle('AJAX...');
       criticalsCard.body('...error');
     }
